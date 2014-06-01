e6644112230ca94380fdcba17c5f1f7c1918463d
jd-erreape <juandediosherrero@gmail.com>
2014-06-01 16:13:09 +0200

    Implemented Rater::Client

--- lib/rater/client.rb
+++ lib/rater/client.rb
@@ -4,6 +4,20 @@ module Rater
       @rated_object = rated_object
     end
 
+    def rates(refresh=false)
+      rated_object.send(:rates, refresh)
+    end
+
+    def add(args)
+      rates.create!(args)
+    end
+
+    def rating
+      return 0 if rates(true).empty?
+
+      rates.map(&:value).inject(0, &:+).to_f / rates.size
+    end
+
     private
 
     def rated_object
--- spec/dummy/app/models/thing.rb
+++ spec/dummy/app/models/thing.rb
@@ -1,3 +1,3 @@
 class Thing < ActiveRecord::Base
-  rated
+  include Rater::Rated
 end
